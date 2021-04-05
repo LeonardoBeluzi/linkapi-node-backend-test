c3917c36e117bd133ed24f6eb2aefd86f53fc544
unknown <leonardobeluzi@gmail.com>
2021-04-05 18:27:52 -0300

    adding sync process

--- src/controllers/DealController.ts
+++ src/controllers/DealController.ts
@@ -1,17 +1,53 @@
 import { NextFunction, Request, Response } from 'express'
 import Deal from '../models/Deal'
+import Sync from '../models/Sync'
+import registerDeal from '../functions/registerDeals'
 
 export default {
     async show(request: Request, response: Response, next: NextFunction) {
         try {
-           const data = await Deal.find()
-           response.send(data) 
+            const data = await Deal.find()
+            const parsedData = data.map((item) => {
+                return {
+                    date: item.date.toLocaleDateString(),
+                    value: item.value
+                }
+            })
+
+            return response.send(parsedData)
         } catch (error) {
-            next(error)     
-        }        
+            next(error)
+        }
     },
 
     async syncData(request: Request, response: Response, next: NextFunction) {
+        try {
+            const data = await Sync.findOne()
 
+            if (!data) {
+                registerDeal.syncData(0)   
+
+                return response.send({
+                    success: true,
+                    message: 'Processing data.'
+                })             
+            }
+
+            if (data.updating) {
+                return response.send({
+                    success: false,
+                    message: 'Processing data, try again later.'
+                })
+            }
+
+            registerDeal.syncData(data.id)
+
+            return response.send({
+                success: true,
+                message: 'Processing data.'
+            })             
+        } catch (error) {
+            next(error)
+        }
     }
 }
--- src/functions/registerDeals.ts
+++ src/functions/registerDeals.ts
@@ -1,9 +1,56 @@
 import piledrive from '../services/piledrive'
 import bling from '../services/bling'
 import xmlParser from './xmlParser'
+import Sync from '../models/Sync'
+import Deal from '../models/Deal'
 
 export default {
     async syncData(lastId: Number) {
-                     
+        //get all deals from piledrive
+        const piledriveData = await piledrive.getDeals(0)
+
+        //lock update requests
+        if (piledriveData.length > 0) {
+            await Sync.updateOne(null, { updating: true }, { upsert: true })
+        }
+
+        try {
+            for (const data of piledriveData) {
+                let id = data.id
+                let wonDate = new Date(new Date(data.first_won_time).toDateString())
+
+                //create deals xml
+                let xml = xmlParser.piledriveDataToXML(data)
+
+                //insert deal on bling
+                let success = await bling.storeDeals(xml)
+
+                //stop sync on error
+                if (success === null) break
+                
+                //ignore existing deal
+                if (success === false) continue
+
+                //update daily value
+                let dailyDeal = await Deal.findOne({ date: wonDate })
+
+                if (dailyDeal) {
+                    await dailyDeal.updateOne({ date: wonDate, value: dailyDeal.value + data.value }, { upsert: true })
+                } else {
+                    dailyDeal = new Deal({ date: wonDate, value: data.value })
+                    await dailyDeal.save()
+                }
+
+                //update last id
+                await Sync.updateOne(null, { lastId: id })
+            }
+        } catch (error) {
+            //unlock update requests
+            console.log(error)
+            await Sync.updateOne(null, { updating: false })
+        }
+
+        //unlock update requests
+        await Sync.updateOne(null, { updating: false })
     }
 }
